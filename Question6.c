4dba5ab4e85975998f8395a831f05ab609952f08
Shazil-we <110455948+Shazil-we@users.noreply.github.com>
2024-10-27 22:56:59 +0500

    Update Question6.c

--- Question6.c
+++ Question6.c
@@ -1,4 +1,23 @@
 #include <stdio.h>
-int main(){
-  
+int matchsticks_to_win(int N) {
+    if (N % 5 == 0) {
+        return -1;
+    } else {
+        return N % 5;
+    }
+}
+int main() {
+    int N;
+    printf("Enter the number of matchsticks: ");
+    scanf("%d", &N);
+
+    int result = matchsticks_to_win(N);
+
+    if (result == -1) {
+        printf("It's impossible for Player A to guarantee a win.\n");
+    } else {
+        printf("Player A should pick %d matchstick(s) on the first turn to guarantee a win.\n", result);
+    }
+
+    return 0;
 }
